0b8830ba6725249bb9ad6b8e18e2ad92ea2ad25c
kmanish1 <[kmanishreddy11@gmail.com]>
2023-03-13 16:50:22 +0530

    add

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ for (let i = 0; i < bars.length; i++) {
         }
     });
 }
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 0, lng: 0 },
+    zoom: 10,
+  });
+
+  // Add a listener for the click event
+  map.addListener("click", (event) => {
+    // Get the clicked location
+    const location = event.latLng.toJSON();
+
+    // Display the location in the address input
+    const addressInput = document.getElementById("address");
+    addressInput.value = `${location.lat}, ${location.lng}`;
+  });
+}
+
+function geocodeAddress() {
+  const geocoder = new google.maps.Geocoder();
+  const address = document.getElementById("address").value;
+
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === "OK") {
+      // Move the map to the first result
+      map.setCenter(results[0].geometry.location);
+      map.setZoom(16);
+
+      // Display the location in the address input
+      const addressInput = document.getElementById("address");
+      addressInput.value = results[0].formatted_address;
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
